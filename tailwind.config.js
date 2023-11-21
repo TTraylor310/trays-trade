45ab146e89e73c59204f8f956fa4bb0cbdb61f12
Tim Traylor <ttraylor310@gmail.com>
2023-11-21 13:50:33 -0800

    basic nextjs template with react redux toolkit, basic building blocks started for app

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // backgroundImage: {
+      // },
+      colors: {
+        mellowBlue: '#D6E4FF',
       },
+      // fontFamily: {
+      // },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 }
